b931d0e61b3a03498f8340b310056526cdd865a2
mariahernandezl <mhernandezlo2022@fau.edu>
2023-06-07 10:25:12 -0400

    Finished home page styling and added html file for about page

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
