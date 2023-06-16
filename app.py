82a9308476e81b2d1341f8c71c6d4100b2003c47
mariahernandezl <mhernandezlo2022@fau.edu>
2023-06-16 15:20:23 -0400

    created json file for geospatial data

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect
 # from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import firebase
+import json 
 
 app = Flask(__name__)
 
--- static/js/map_script.js
+++ static/js/map_script.js
@@ -2,43 +2,36 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 17,
-    center: { lat: 37.704638, lng: -89.465320},
+    zoom: 40,
+    center: { lat: 27.5293081, lng: -80.3512673},
+    mapTypeId: 'satellite',
+    tilt:0
   });
   // Load GeoJSON.
-  map.data.loadGeoJson(
-    "https://storage.googleapis.com/mapsdevsite/json/google.json"
-  );
+  map.data.loadGeoJson(href = 'api.jsonserve.com/AAUwWg');
   // Color each letter gray. Change the color when the isColorful property
   // is set to true.
-  map.data.setStyle((feature) => {
-    let color = "gray";
-
-    if (feature.getProperty("isColorful")) {
-      color = feature.getProperty("color");
-    }
-    return /** @type {!google.maps.Data.StyleOptions} */ {
-      fillColor: color,
-      strokeColor: color,
-      strokeWeight: 2,
-    };
+  map.data.setStyle({
+    fillColor: '#2687bf',
+    fillOpacity: .3,
+    strokeWeight: 0
   });
   // When the user clicks, set 'isColorful', changing the color of the letters.
-  map.data.addListener("click", (event) => {
-    event.feature.setProperty("isColorful", true);
-  });
+  //map.data.addListener("click", (event) => {
+  //  event.feature.setProperty("isColorful", true);
+  //});
   // When the user hovers, tempt them to click by outlining the letters.
   // Call revertStyle() to remove all overrides. This will use the style rules
   // defined in the function passed to setStyle()
-  map.data.addListener("mouseover", (event) => {
-    map.data.revertStyle();
-    map.data.overrideStyle(event.feature, { strokeWeight: 8 });
-  });
-  map.data.addListener("mouseout", (event) => {
-    map.data.revertStyle();
-  });
+ // map.data.addListener("mouseover", (event) => {
+ //   map.data.revertStyle();
+ //   map.data.overrideStyle(event.feature, { strokeWeight: 8 });
+ // });
+  //map.data.addListener("mouseout", (event) => {
+  //  map.data.revertStyle();
+ // });
+  
 }
 
-console.log("started js file");
 window.initMap = initMap;
 
