221b5d14bc992a04e722276078e59e1b9d9fda9f
wfairmanOlin <wfairman@olin.edu>
2024-04-02 09:12:56 -0400

    updated drone page

--- app.py
+++ app.py
@@ -62,19 +62,6 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/HAUCS')
-def haucs():
-    last_do = get_all_do()
-    with open('static/json/farm_features.json', 'r') as file:
-        data = file.read()
-    
-    return render_template('HAUCS.html', data=data, do_values=json.dumps(last_do))
-
-@app.route('/recent')
-def recent():
-    data = db.reference("/LH_Farm/recent").get()
-    return render_template('haucs_recent.html', keys=reversed(data.keys()), data=data)
-
 @app.route('/biomass')
 def bmass():
     last_battv = get_all_battv()
@@ -89,31 +76,6 @@ def drone():
     print(data)
     return render_template('drone.html', data=data)
 
-@app.route('/sensor'+'<int:sensor_id>')
-def show_sensor(sensor_id):
-    bmx = firebase.bmass_sensor(sensor_id, 600)
-    last_battv = bmx.battv[-1]
-    last_dt = bmx.s_dt[-1]
-    str_date = last_dt.strftime('%A, %B %d')
-    str_time = last_dt.strftime('%I:%M %p')
-    bmx.plot_timeseries(mv=10)
-    return render_template('tanks_analytics.html', sensor_id=sensor_id, last_date=str_date, last_time = str_time, last_battv=last_battv, last_dt=last_dt)
-
-@app.route('/pond'+'<int:pond_id>')
-def show_pond(pond_id):
-    pondx = firebase.pond(pond_id, 48)
-    last_do = pondx.do[-1]
-    last_temp = round(pondx.temp[-1],2)
-    last_dt = pondx.d_dt[-1]
-    str_date = last_dt.strftime('%A, %B %d')
-    str_time = last_dt.strftime('%I:%M %p')
-    pondx.plot_temp_do(mv=10)
-    return render_template('haucs_analytics.html', pond_id=pond_id, last_date=str_date, last_time = str_time,last_do=last_do, last_temp = last_temp)
-
-@app.route('/feedback')
-def feedback():
-    return render_template('feedback.html',ponds=ponds)
-
 @app.route('/eggs')
 def eggs():
     egg = firebase.egg_sensor(800)
@@ -126,6 +88,48 @@ def eggs():
     egg.plot_prediction()
     egg.plot_peakDetection()
     return render_template('eggs.html',  last_date=str_date, last_time=str_time, last_refresh = current_time)
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html',ponds=ponds)
+
+@app.route('/HAUCS')
+def haucs():
+    last_do = get_all_do()
+    with open('static/json/farm_features.json', 'r') as file:
+        data = file.read()
+    
+    return render_template('HAUCS.html', data=data, do_values=json.dumps(last_do))
+
+@app.route('/pond'+'<int:pond_id>')
+def show_pond(pond_id):
+    pondx = firebase.pond(pond_id, 48)
+    last_do = pondx.do[-1]
+    last_temp = round(pondx.temp[-1],2)
+    last_dt = pondx.d_dt[-1]
+    str_date = last_dt.strftime('%A, %B %d')
+    str_time = last_dt.strftime('%I:%M %p')
+    pondx.plot_temp_do(mv=10)
+    return render_template('haucs_analytics.html', pond_id=pond_id, last_date=str_date, last_time = str_time,last_do=last_do, last_temp = last_temp)
+
+@app.route('/recent')
+def recent():
+    data = db.reference("/LH_Farm/recent").get()
+    return render_template('haucs_recent.html', keys=reversed(data.keys()), data=data)
+
+@app.route('/sensor'+'<int:sensor_id>')
+def show_sensor(sensor_id):
+    bmx = firebase.bmass_sensor(sensor_id, 600)
+    last_battv = bmx.battv[-1]
+    last_dt = bmx.s_dt[-1]
+    str_date = last_dt.strftime('%A, %B %d')
+    str_time = last_dt.strftime('%I:%M %p')
+    bmx.plot_timeseries(mv=10)
+    return render_template('tanks_analytics.html', sensor_id=sensor_id, last_date=str_date, last_time = str_time, last_battv=last_battv, last_dt=last_dt)
+
+
+
+
 
 if __name__ == "__main__":
     if not deployed:
