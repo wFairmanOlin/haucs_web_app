56794095f46f4cf58d84125ee392cd18d4d98a36
wfairmanOlin <wfairman@olin.edu>
2024-10-04 16:12:05 -0400

    updated pond title, improved weather response time, add redirect to history & pond pages

--- static/js/history.js
+++ static/js/history.js
@@ -1,46 +1,3 @@
-function convert_to_mgl(do_input, t, p, s){
-    //do: dissolved oxygen in percent saturation
-    //t: temperature in celcius
-    //p: pressure in hPa
-    //s: salinity in parts per thousand
- 
-    T = t + 273.15; //temperature in kelvin
-    P = p * 9.869233e-4; //pressure in atm
-
-    DO_baseline = Math.exp(-139.34411 + 1.575701e5/T - 6.642308e7/Math.pow(T, 2) + 1.2438e10/Math.pow(T, 3) - 8.621949e11/Math.pow(T, 4));
-
-    Fs = Math.exp(-s * (0.017674 - 10.754/T + 2140.7/Math.pow(T, 2)));
-
-    theta = 0.000975 - 1.426e-5 * t + 6.436e-8 * Math.pow(t, 2);
-    u = Math.exp(11.8571 - 3840.7/T - 216961/Math.pow(T, 2));
-    Fp = (P - u) * (1 - theta * P) / (1 - u) / (1 - theta);
-
-    DO_corrected = DO_baseline * Fs * Fp;
-    DO_mgl = do_input / 100 * DO_corrected;
-
-    return DO_mgl;
-}
-
-function getStartTime(delta) {
-    var start = new Date();
-    start.setHours(start.getHours() - delta);
-    let year = start.getUTCFullYear();
-    let month = start.getUTCMonth() + 1;
-    if (month < 10)
-        month = "0" + month;
-    let day = start.getUTCDate();
-    if (day < 10)
-        day = "0" + day;
-    let hour = start.getUTCHours();
-    if (hour < 10)
-        hour = "0" + hour;
-    let minute = start.getUTCMinutes();
-    if (minute < 10)
-        minute = "0" + minute;
-    let startString = '' + year + month + day + '_' + hour + ':' + minute + ':00';
-    return startString;
-}
-
 function initializeCombobox(jsonData) {
     const pondSelector = document.getElementById('pondSelector');
     pondSelector.innerHTML = '<option value="all" selected>All Ponds</option>'; // Default option
@@ -68,7 +25,7 @@ function generateTable(data) {
         // item.datetime.toIsoString().slice(0, 19).replace('T', ' ')
         row.innerHTML = `
             <td>${item.datetime.toLocaleTimeString("en-US", {month:"2-digit",day:"2-digit",hour:"2-digit", minute:"2-digit", timeZoneName: "short"})}</td>
-            <td>${item.pond_id}</td>
+            <td onclick="window.location='/pond' + ${item.pond_id}">${item.pond_id}</td>
             <td>${item.do_mgl.toFixed(2)}</td>
             <td>${item.avg_do.toFixed(1)}</td>
             <td>${item.avg_temp.toFixed(1)}</td>
